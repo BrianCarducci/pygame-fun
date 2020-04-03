23e145ff41ec0a7acf4849b30e3870214aa73e7d
BrianCarducci <briancarducci@gmail.com>
2020-04-03 00:17:14 -0400

    refactored checking player action to method in player class

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pygame
-# from entities import *
+from entities import player
 pygame.init()
 
 SCREEN_WIDTH = 500
@@ -15,16 +15,15 @@ x = 250
 y = SCREEN_HEIGHT - height
 vel = 15
 
-is_jump = False
-jump_count = 10
-jump_coeff = -1
+the_player = player.Player(x, y, width, height, vel, False, 10)
+entities = [the_player]
 
-entities = Player()
 
-# def draw(entities):
-#     for entity in entities:
-#         win.fill((0, 0, 0))
-#         pygame.draw.
+def draw(entities):
+    for entity in entities:
+        win.fill((0, 0, 0))
+        pygame.draw.rect(win, (255, 0, 0), (entity.x, entity.y, entity.width, entity.height))
+        pygame.display.update()
 
 
 run = True
@@ -36,26 +35,7 @@ while run:
             run = False
 
     keys = pygame.key.get_pressed()
-
-    if keys[pygame.K_LEFT] or keys[pygame.K_a]:
-        if x > vel:
-            x -= vel
-    if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
-        if x <= SCREEN_WIDTH - width - vel:
-            x += vel
-    if not is_jump:
-        if keys[pygame.K_SPACE]:
-            is_jump = True
-    else:
-        if jump_count >= -10:
-            y -= jump_count
-            jump_count -= 1
-        else:
-            is_jump = False
-            jump_count = 10
-
-    win.fill((0, 0, 0))
-    pygame.draw.rect(win, (255, 0, 0), (x, y, width, height))
-    pygame.display.update()
+    the_player.check_player_actions(keys, "playing", SCREEN_WIDTH)
+    draw(entities)
 
 pygame.quit()
